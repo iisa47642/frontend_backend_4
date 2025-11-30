8455c2c9df131f081e627187059951fad879581c
iisa47642 <136353118+iisa47642@users.noreply.github.com>
2025-11-30 20:31:28 +0300

    first

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/frontend_and_backend_4/'
+  base: '/frontend_backend_4/'
 })
